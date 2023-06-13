aee6cdb85b5039717a0f315d24600ed7360c2a44
ssh00n <seunghunshin1284@gmail.com>
2023-06-13 15:01:42 +0900

    feat: User API 수정

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { User } from "../models/User";
+import User from "../models/User";
 
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
