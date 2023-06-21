443822e30b091b4944f274a3788ecf2c028c3bf4
yjh4124 <yjh4124@gmail.com>
2023-06-21 18:59:17 +0900

    feat: 대화 후 리포트 추가

--- client/src/scenes/Airport.ts
+++ client/src/scenes/Airport.ts
@@ -260,112 +260,115 @@ export default class AirportScene extends Phaser.Scene {
             if(valve_E===true){
                 if (this.isAudioPlaying) {
                 return;
-            }
-            for (let npcInfo of this.npcList) {
-                if (Phaser.Math.Distance.Between(this.player1!.x, this.player1!.y, npcInfo.x, npcInfo.y) < 100) {
+                }
+                for (let npcInfo of this.npcList) {
+                    if (Phaser.Math.Distance.Between(this.player1!.x, this.player1!.y, npcInfo.x, npcInfo.y) < 100) {
 
-                        this.player1!.setVelocity(0, 0);
-                    this.player1!.anims.play(`${this.player1!.texture.key}_idle_down`, true);
-                        store.dispatch(openNPCDialog());
-    
-                    this.cursors!.left.enabled = false;
-                        this.cursors!.right.enabled = false;
-                        this.cursors!.up.enabled = false;
-                    this.cursors!.down.enabled = false;
-    
-                            if (this.socket2 === null || this.socket2 === undefined) {
-                            this.socket2 = io(`${serverUrl}/interaction`);
-                            this.socket2.on("connect", () => {
-                                this.interacting = true;
-                                console.log("connect, interaction socket.id: ", this.socket2!.id);
-                                this.socket2!.on("speechToText", (response: string) => {
-                                    console.log("USER: ", response);
-                                    store.dispatch(appendMessage({
-                                        name: this.userNickname,
-                                        img: this.playerTexture,
-                                    // img: "",
-                                        side: "right",
-                                        text: response
-                                    }));
-    
-                                });
-                                this.socket2!.on("npcResponse", (response: string) => {
-                                    console.log("NPC: ", response);
-                                    store.dispatch(appendMessage({
-                                        name: npcInfo.name,
-                                        img: npcInfo.texture,
-                                    // img: "",
-                                        side: "left",
-                                        text: response
-                                    }));
-    
-                                });
-                                this.socket2!.on("totalResponse", (response: any) => {
-                                    console.log("totalResponse event response: ", response);
-                                    // this.isAudioPlaying = true;
-                                const audio = new Audio(response.audioUrl);
-                                    audio.onended = () => {
-                                    console.log("audio.onended");
-                                    this.isAudioPlaying = false;
-                                };
-                                audio.play();
-                                });
-                                this.socket2!.on(
-                                "recommendedResponses",
-                                (responses: string[]) => {
-                                    console.log(
-                                        "Recommended Responses: ",
-                                        responses
-                                    );
-                                    // TODO : Store에 SentenceBox 상태정의하고 dispatch
-                                    store.dispatch(clearSentences());
-                                    responses.forEach((response, index) => {
-                                        store.dispatch(
-                                            appendSentence({
-                                                _id: index.toString(),
-                                                sentence: response,
-                                            })
-                                        );
-                                    });
-                                }
-                            );
-                        });
-                        }
-                        else { // 이미 소켓이 연결되어 있는데 다시 한번 E키를 누른 경우
                             this.player1!.setVelocity(0, 0);
-                        this.player1!.setPosition(this.player1!.x, this.player1!.y);
+                        this.player1!.anims.play(`${this.player1!.texture.key}_idle_down`, true);
+                            store.dispatch(openNPCDialog());
+        
+                        this.cursors!.left.enabled = false;
+                            this.cursors!.right.enabled = false;
+                            this.cursors!.up.enabled = false;
+                        this.cursors!.down.enabled = false;
+        
+                                if (this.socket2 === null || this.socket2 === undefined) {
+                                this.socket2 = io(`${serverUrl}/interaction`);
+                                this.socket2.on("connect", () => {
+                                    this.interacting = true;
+                                    console.log("connect, interaction socket.id: ", this.socket2!.id);
+                                    this.socket2!.on("speechToText", (response: string) => {
+                                        console.log("USER: ", response);
+                                        store.dispatch(appendMessage({
+                                            name: this.userNickname,
+                                            img: this.playerTexture,
+                                        // img: "",
+                                            side: "right",
+                                            text: response
+                                        }));
+        
+                                    });
+                                    this.socket2!.on("npcResponse", (response: string) => {
+                                        console.log("NPC: ", response);
+                                        store.dispatch(appendMessage({
+                                            name: npcInfo.name,
+                                            img: npcInfo.texture,
+                                        // img: "",
+                                            side: "left",
+                                            text: response
+                                        }));
+        
+                                    });
+                                    this.socket2!.on("totalResponse", (response: any) => {
+                                        console.log("totalResponse event response: ", response);
+                                        // this.isAudioPlaying = true;
+                                    const audio = new Audio(response.audioUrl);
+                                        audio.onended = () => {
+                                        console.log("audio.onended");
+                                        this.isAudioPlaying = false;
+                                    };
+                                    audio.play();
+                                    });
+                                    this.socket2!.on(
+                                    "recommendedResponses",
+                                    (responses: string[]) => {
+                                        console.log(
+                                            "Recommended Responses: ",
+                                            responses
+                                        );
+                                        // TODO : Store에 SentenceBox 상태정의하고 dispatch
+                                        store.dispatch(clearSentences());
+                                        responses.forEach((response, index) => {
+                                            store.dispatch(
+                                                appendSentence({
+                                                    _id: index.toString(),
+                                                    sentence: response,
+                                                })
+                                            );
+                                        });
+                                    }
+                                );
+                            });
+                            }
+                            else { // 이미 소켓이 연결되어 있는데 다시 한번 E키를 누른 경우
+                                this.player1!.setVelocity(0, 0);
+                            this.player1!.setPosition(this.player1!.x, this.player1!.y);
 
-                        this.cursors!.left.isDown = false;
-                        this.cursors!.right.isDown = false;
-                        this.cursors!.up.isDown = false;
-                            this.cursors!.down.isDown = false;
+                            this.cursors!.left.isDown = false;
+                            this.cursors!.right.isDown = false;
+                            this.cursors!.up.isDown = false;
+                                this.cursors!.down.isDown = false;
 
-                        this.cursors!.left.enabled = true;
-                            this.cursors!.right.enabled = true;
-                        this.cursors!.up.enabled = true;
-                            this.cursors!.down.enabled = true;
-    
-                            this.interacting = false;
-                            this.socket2?.disconnect();
-                            this.socket2 = null;
-                        // store.dispatch(clearMessages());
-                        // store.dispatch(openAirport());
-                        store.dispatch(openReport());
-                        valve_E=false
+                            this.cursors!.left.enabled = true;
+                                this.cursors!.right.enabled = true;
+                            this.cursors!.up.enabled = true;
+                                this.cursors!.down.enabled = true;
+        
+                                this.interacting = false;
+                                this.socket2?.disconnect();
+                                this.socket2 = null;
+                            // store.dispatch(clearMessages());
+                            // store.dispatch(openAirport());
+                            store.dispatch(openReport());
+                            valve_E=false
+                        }
+
+                        break;
                     }
                 }
             }
             else{
                     store.dispatch(clearMessages());
-                        store.dispatch(clearSentences());
+                    store.dispatch(clearSentences());
                     store.dispatch(openAirport());
                 valve_E=true
-                }
-                    break;
-                }
+            }
+                    
+            }
                         
 
-        });
+        );
         // 녹음 데이터를 보내고 응답을 받는 키 설정
         this.input.keyboard!.on("keydown-R", async () => {
             if (this.isAudioPlaying) {
