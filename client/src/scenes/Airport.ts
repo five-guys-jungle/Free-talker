b577dee2e1eaff8f4dba5b59340bfbbaa990ffd2
poter0501 <poter0501@gmail.com>
2023-06-22 15:52:23 +0900

    Feat:자유의 여신상 추가

--- client/src/scenes/Airport.ts
+++ client/src/scenes/Airport.ts
@@ -600,5 +600,27 @@ export default class AirportScene extends Phaser.Scene {
         };
         npc1.sprite = this.physics.add.sprite(npc1.x, npc1.y, npc1.texture);
         this.npcList.push(npc1);
+
+        let npc2: npcInfo = {
+            name: "statueOfLiberty",
+            x: 2150,
+            y: 1430,
+            texture: "statueOfLiberty",
+            sprite: null,
+        };
+        npc2.sprite = this.physics.add.sprite(npc2.x, npc2.y, npc2.texture);
+        npc2.sprite.setScale(0.35);
+        // this.npcList.push(npc2);
+
+        let npc3: npcInfo = {
+            name: "statueOfLiberty",
+            x: 2030,
+            y: 1430,
+            texture: "statueOfLiberty2",
+            sprite: null,
+        };
+        npc3.sprite = this.physics.add.sprite(npc3.x, npc3.y, npc3.texture);
+        npc3.sprite.setScale(0.35);
+        // this.npcList.push(npc3);
     }
 }
