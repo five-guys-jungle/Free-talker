521a2c6ba045e92205d302d266bb204021f95e0e
ssh00n <seunghunshin1284@gmail.com>
2023-07-01 16:14:43 +0900

    fix: 역할 변수 명 수정

--- server/models/Role.ts
+++ server/models/Role.ts
@@ -11,25 +11,25 @@ interface preDefinedRoleDictionary {
 
 export const preDefinedRole: preDefinedRoleDictionary = {};
 
-preDefinedRole["chairMart"] = {
+preDefinedRole["Mart"] = {
   role: ["Cashier", "Customer"],
-  situation: "a first-time customer at the grocery store looking for specific items and discounts", 
+  situation: "a first-time customer at the grocery store looking for specific items and discounts",
   recommendations: [
     [
-    "Hello, how can I assist you today?",
-    "Our organic produce section is right at the back of the store, next to the dairy products",
-    "Yes, indeed! All our dairy products are 15% off this week. Don't forget to grab your favorites",
-    "Found everything you were looking for?",
-    "Your total is $56.78. Do you have our loyalty card for an extra discount?",
-   ],
-   [
-    "I'm looking for some organic produce. Can you point me in the right direction?",
-    "Great, thank you. I also heard there's a discount on dairy products this week. Is that still on?",
-    "That's wonderful! I'll definitely check that out.",
-    "Yes, thanks to your help. How much is my total?",
-    "I don't, but I'd like to sign up for one."
-   ]
-]
+      "Hello, how can I assist you today?",
+      "Our organic produce section is right at the back of the store, next to the dairy products",
+      "Yes, indeed! All our dairy products are 15% off this week. Don't forget to grab your favorites",
+      "Found everything you were looking for?",
+      "Your total is $56.78. Do you have our loyalty card for an extra discount?",
+    ],
+    [
+      "I'm looking for some organic produce. Can you point me in the right direction?",
+      "Great, thank you. I also heard there's a discount on dairy products this week. Is that still on?",
+      "That's wonderful! I'll definitely check that out.",
+      "Yes, thanks to your help. How much is my total?",
+      "I don't, but I'd like to sign up for one."
+    ]
+  ]
 }
 
 
