aa710858f3ad65f04c57191d9813c2bb49e9ca30
yjh4124 <yjh4124@gmail.com>
2023-07-04 23:06:40 +0900

    fix: 도움말들 항상 최상단에 위치

--- client/src/components/Game.tsx
+++ client/src/components/Game.tsx
@@ -45,13 +45,13 @@ const Game = () => {
         {mode === FREEDIALOG && <FreeDialog />}
         {mode === REPORT && <Report />}
         {(mode === AIRPORT || mode === USA) && <ReportBook />}
-        {mode !== NPCDIALOG && mode !== USERDIALOG && mode !== FREEDIALOG && (
+        {/* {mode !== NPCDIALOG && mode !== USERDIALOG && mode !== FREEDIALOG && ( */}
             <ButtonContainer>
                 <LevelButton />
                 <Guider />
                 <KeyGuider />
             </ButtonContainer>
-        )}
+        {/* )} */}
 
     </BackgroundDiv>;
 };
@@ -72,4 +72,5 @@ const ButtonContainer = styled.div`
   display: flex;
   flex-direction: row;
   gap: 10px; /* Add gap to separate the buttons */
+  z-index:3;
 `;
